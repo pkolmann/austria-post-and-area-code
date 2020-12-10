8945d33ae5ee960420021745bdbc17ec8fb5368b
Philipp Kolmann <philipp@kolmann.at>
2020-12-10 14:20:58 +0100

    update title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de-AT">
 <head>
-    <title>Ortsvorwahlen Österreich</title>
+    <title>Ortsvorwahlen und Postleitzahlen in Österreich</title>
 
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
